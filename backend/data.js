6ca6e74f8c18d0449c3867a2de84dad7366f0450
kevalparekh2611 <keval.parekh2611@gmail.com>
2021-01-04 10:49:19 +0530

    created server side

---
+++ backend/data.js
@@ -0,0 +1,103 @@
+const data = {
+
+    products:[
+        {
+            _id : '1',
+            name : 'Grey Sofa Bed',
+            category : 'Sofa',
+            image : 'images/p1.jpg',
+            price : 40000,
+            countStock : 10,
+            brand : 'Godrej Interio',
+            rating : 4.5,
+            numReviews : 10,
+            description : 'Two in one stylish sofa bed. Comfertable and light.'
+        },
+        {
+            _id : '2',
+            name : 'Remmy Sofa',
+            category : 'Sofa',
+            image : 'images/p2.jpg',
+            price : 10000,
+            countStock : 10,
+            brand : 'Pepperfry',
+            rating : 4.5,
+            numReviews : 12,
+            description : 'Comfertable and classy two seater sofa'
+        },
+        {
+            _id : '3',
+            name : 'Austin Sofa',
+            category : 'Sofa',
+            image : 'images/p3.jpg',
+            price : 15000,
+            countStock : 0,
+            brand : 'Durian',
+            rating : 5,
+            numReviews : 15,
+            description : 'Stylish and authentic leather three seater sofa.'
+        },
+        {
+            _id : '4',
+            name : 'Diner Dan',
+            category : 'Dinning Table',
+            image : 'images/p4.jpg',
+            price : 17000,
+            countStock : 10,
+            brand : 'Pepperfry',
+            rating : 4.5,
+            numReviews : 10,
+            description : 'Stylish and classy, glass dinning table. White.'
+        },
+        {
+            _id : '5',
+            name : 'Sal Wood Diners',
+            category : 'Dinning Table',
+            image : 'images/p5.jpg',
+            price : 15000,
+            countStock : 10,
+            brand : 'WoodStreet',
+            rating : 4,
+            numReviews : 10,
+            description : 'Asthetic dinner table. Solid wood wqater proof.'
+        },
+        {
+            _id : '6',
+            name : 'Milan Bed',
+            category : 'Bed',
+            image : 'images/p6.jpg',
+            price : 20000,
+            countStock : 10,
+            brand : 'Nilkamal',
+            rating : 4.5,
+            numReviews : 15,
+            description : 'Solid wood bed with storage space. Fine finish.'
+        },
+        {
+            _id : '7',
+            name : 'Boss Table',
+            category : 'Office/Study Table',
+            image : 'images/p7.jpg',
+            price : 14000,
+            countStock : 10,
+            brand : 'Nilkamal',
+            rating : 5,
+            numReviews : 12,
+            description : 'Solid wood office Table. Classy black.'
+        },
+        {
+            _id : '8',
+            name : 'Bed side store',
+            category : 'Storage Cabinet',
+            image : 'images/p8.jpg',
+            price : 18000,
+            countStock : 10,
+            brand : 'WoodStreet',
+            rating : 4,
+            numReviews : 8,
+            description : 'Sag wood storage cabinet. 3 stack. Fine Black.'
+        }
+    ]
+}
+
+export default data;

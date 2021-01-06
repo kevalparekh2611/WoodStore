c1449f9dab49a46cb0f0a439c6dfd0270866dcbf
kevalparekh2611 <keval.parekh2611@gmail.com>
2021-01-06 11:05:03 +0530

    Redux implemented

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
+
+const initialState = {};
+const reducer = combineReducers({
+    productList : productListReducer,
+})
+
+const composeEnhance = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer,
+    initialState , 
+    composeEnhance(applyMiddleware(thunk))
+    );
+
+export default store;

9e3682fb6571e6982f957c42beaea09d2fc6aeae
kevalparekh2611 <keval.parekh2611@gmail.com>
2021-01-06 13:36:20 +0530

    details connected to backend

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,76 +1,90 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data';
 
 function ProductScreen(props) {
-    const product = data.products.find( x => x._id === props.match.params.id);
-    if(!product){
-        return(<div> Product doesnt exist</div>);
-    }
 
+    const productDetails = useSelector( (state) => state.productDetails);
+    const {loading , error , product} = productDetails;
+    const productId = props.match.params.id; 
+    
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(detailsProduct(productId));
+    },[dispatch,productId])
     return (
         <div>
-            <Link to='/'> BACK </Link>
-            <hr></hr>
-           <div className='row top'>
-               <div className='col-2'>
-                    <img className='large' src={`../${product.image}`} alt={product.name}></img>
-               </div>
-               <div className='col-1'>
-                   <ul>
-                       <li>
-                           <h1>{product.name}</h1>
-                       </li>
-                       <li>
-                            <Rating 
-                                rating = {product.rating}
-                                numReviews = {product.numReviews}>
+            {
+                loading ?( <LoadingBox></LoadingBox> ):
+                error ? (<MessageBox varient="problem"> {error} </MessageBox>):
+                <div>
+                    <Link to='/'> BACK </Link>
+                    <hr></hr>
+                <div className='row top'>
+                    <div className='col-2'>
+                            <img className='large' src={`../${product.image}`} alt={product.name}></img>
+                    </div>
+                    <div className='col-1'>
+                        <ul>
+                            <li>
+                                <h1>{product.name}</h1>
+                            </li>
+                            <li>
+                                    <Rating 
+                                        rating = {product.rating}
+                                        numReviews = {product.numReviews}>
 
-                            </Rating>
-                       </li>
-                       <li>
-                           Price : Rs. {product.price}
-                       </li>
-                       <li>
-                           Description :
-                           <p>
-                               {product.description}
-                           </p>
-                       </li>
-                   </ul>
-               </div>
-               <div className='col-1'>
-                   <div className='card cardbody'>
-                       <ul>
-                           <li>
-                               <div className='row '>
-                                   <div>Price </div>
-                                   <div className='largePrice'> Rs. {product.price}</div>
-                               </div>
-                           </li>
-                           <li>
-                               <div className='row '>
-                                   <div> Status </div>
-                                   <div>
-                                       {
-                                           product.countStock > 0 ? (
-                                               <span className='success'> Product In Stock </span>
-                                           ):(
-                                                <span className = 'error'> Unavailable </span>
-                                           )
-                                       }
-                                   </div>
-                               </div>
-                           </li>
-                           <li>
-                               <button className='purchase block'> Add to Cart </button>
-                           </li>
-                       </ul>
-                   </div>
-               </div>
-           </div>
+                                    </Rating>
+                            </li>
+                            <li>
+                                Price : Rs. {product.price}
+                            </li>
+                            <li>
+                                Description :
+                                <p>
+                                    {product.description}
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='col-1'>
+                        <div className='card cardbody'>
+                            <ul>
+                                <li>
+                                    <div className='row '>
+                                        <div>Price </div>
+                                        <div className='largePrice'> Rs. {product.price}</div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className='row '>
+                                        <div> Status </div>
+                                        <div>
+                                            {
+                                                product.countStock > 0 ? (
+                                                    <span className='success'> Product In Stock </span>
+                                                ):(
+                                                        <span className = 'error'> Unavailable </span>
+                                                )
+                                            }
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <button className='purchase block'> Add to Cart </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            } 
         </div>
+        
     );
 }
 
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,5 +1,8 @@
 import 
-{   PRODUCT_LIST_FAILURE, 
+{   PRODUCT_DETAILS_FAILURE,
+PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+PRODUCT_LIST_FAILURE, 
     PRODUCT_LIST_REQUEST, 
     PRODUCT_LIST_SUCCESS 
 } from "../constants/productConstants";
@@ -11,6 +14,19 @@ import
         case PRODUCT_LIST_SUCCESS :
             return {loading : false , products : action.payload};
         case PRODUCT_LIST_FAILURE :
+            return {loading : false , error : action.payload};
+        default :
+            return state;
+    }    
+}
+
+export const productDetailsReducer = (state = { loading : true, product : {}},action) => {
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST :
+            return {loading : true};
+        case PRODUCT_DETAILS_SUCCESS :
+            return {loading : false , product : action.payload};
+        case PRODUCT_DETAILS_FAILURE :
             return {loading : false , error : action.payload};
         default :
             return state;
